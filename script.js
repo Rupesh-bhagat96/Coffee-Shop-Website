de9de2aaa27b1cfa53c59426ab9992ceb4329ec5
Rupesh-bhagat96 <rupeshnbhagat96@gmail.com>
2024-10-06 09:02:07 +0530

    header section complete

---
+++ script.js
@@ -0,0 +1,28 @@
+const menuBtn=document.querySelector(".menu-icon")
+const navBar=document.querySelector(".navbar")
+const inputSearch=document.querySelector(".header .searchinputs")
+const searchBtn=document.querySelector(".search-icon")
+const cartBtn=document.querySelector(".cart-icon")
+const cartContainer=document.querySelector(".header .cartitems")
+
+
+
+menuBtn.addEventListener('click',()=>{
+    navBar.classList.toggle("active") 
+    inputSearch.classList.remove("active") 
+    cartContainer.classList.remove("active")
+
+})
+
+searchBtn.addEventListener('click',()=>{
+    inputSearch.classList.toggle("active")
+    navBar.classList.remove("active") 
+    cartContainer.classList.remove("active")
+    
+})
+cartBtn.addEventListener('click',()=>{
+    cartContainer.classList.toggle("active")
+    navBar.classList.remove("active") 
+    inputSearch.classList.remove("active") 
+    
+})
